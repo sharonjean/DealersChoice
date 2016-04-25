bc8c8fd3d51e5c07ae4da9ddce56cf332a67903f
Ian Thorp <IanMThorp@gmail.com>
2016-04-25 07:36:59 -0700

    Test for heroku 500 error

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -4,22 +4,25 @@ class DrinksController < ApplicationController
     # Need to recieve params[:choice_id](drink selected id).
 
     if session[:drink_ids] == nil
+      p "THIS IS IN THE SESSION CREATION PART"
       session[:drink_ids] = params[:id]
     end
 
     if params[:choice_id]
+      p "THIS IS IN THE CHOICE ID STATEMENT"
       # If drink is chosen, return json of the drink
       @drink = Drink.find(params[:choice_id])
       params[:choice_id].clear
       render json: @drink
     elsif session[:drink_ids].length > 0
+      p "THIS IS IN THE DRINK ID STATEMENT"
       # if drink is not chosen, return next drink
       @drink_id = session[:drink_ids].shuffle!.pop
       @drink = Drink.find(@drink_id)
       render json: @drink
     else
       # Need something for edge case if they don't like any drinks
-      redirect_to drinks_index_path
+      redirect_to root_path
     end
   end
 
