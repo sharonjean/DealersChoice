28627834b439421cf54083f6721403877f04b001
Ian Thorp <IanMThorp@gmail.com>
2016-04-25 11:05:43 -0700

    Remove session based logic for param based

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,20 +3,22 @@ class DrinksController < ApplicationController
   def choose
     # Need to recieve params[:choice_id](drink selected id).
 
-    if session[:drink_ids] == nil
-      session[:drink_ids] = params[:id]
+    if params[:drink_ids] == nil
+      params[:drink_ids] = params[:id]
     end
-
+    # params[:drink_ids]
+    p 'HERE IT IS RIGHT ABOVE AND BELOW'
+    p params[:id]
     if params[:choice_id]
       # If drink is chosen, return json of the drink
       @drink = Drink.find(params[:choice_id])
       params[:choice_id].clear
       redirect_to bar_drink_path(id: @drink.id, bar_id: @drink.bar_id)
-    elsif session[:drink_ids].length > 0
+    elsif params[:drink_ids].length > 0
       # if drink is not chosen, return next drink
-      @drink_id = session[:drink_ids].shuffle!.pop
+      @drink_id = params[:drink_ids].shuffle!.pop
       @drink = Drink.find(@drink_id)
-      render json: @drink
+      render json: {current_drink: @drink, drink_ids: params[:drink_ids]}
     else
       # Need something for edge case if they don't like any drinks
       redirect_to root_path
