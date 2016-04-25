527e216c0a1732bd856859ca1514afe5b9c82e39
Ian Thorp <IanMThorp@gmail.com>
2016-04-25 16:23:58 -0700

    Refactor logic for tags to pass less data

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,36 +9,27 @@ class TagsController < ApplicationController
     end
 
     @selected_tag = Tag.find(params[:id])
-    if params[:id] == nil
-      params[:id] = []
-    end
-    if params[:tags]
-      @previous_tags = params[:tags]
-      @previous_tags.push(@selected_tag.id)
-    else
-      @previous_tags = []
-      @previous_tags.push(@selected_tag.id)
-    end
-    if @previous_tags.length > 0
-    @next_tags = { children: @selected_tag.children, tags: @previous_tags }
-    else
-      @next_tags = { children: @selected_tag.children}
-    end
+    @next_tags = @selected_tag.children
 
     # Drink choosing logic
-    @drink_ids = []
-    if @next_tags[:children].length === 0
-      @drink_choices = []
+
+    if @next_tags.length === 0
+      @drink_ids = []
+      @previous_tags = [@selected_tag.id]
+      @current_tag = @selected_tag
+      while @current_tag.parents.length > 0
+        @current_tag = @current_tag.parents.first
+        @previous_tags << @current_tag.id
+      end
       @bars_drinks = Drink.all
       @bars_drinks.each do |drink|
         @drink_tags_array = drink.tags.pluck(:id)
-        @in_common = @drink_tags_array & @previous_tags.map!{|num| num.to_i}
+        @in_common = @drink_tags_array & @previous_tags
 
         if @in_common.length >= @previous_tags.length - 1
           @drink_ids << drink.id
         end
       end
-      # params[:tags] = []
 
       redirect_to :action => "choose", :controller => "drinks", :id => @drink_ids
     else
