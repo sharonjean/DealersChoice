9b883fda6adda966815ca79041aca1778db8f222
colette-pitamba <colette.prepont@gmail.com>
2016-04-26 12:12:18 -0700

    create nested route and edit sessions controller to return user token

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+	devise_scope :user do
+		post '/auth/sign_in', to: 'users/sessions#create'
+	end
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :bars, except: [:new, :edit] do
     resources :drinks, except: [:new, :edit]
